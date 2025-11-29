779880e3386648879319045d8ea23336541231c3
kovacslevente2 <kovacslevente2315@gmail.com>
2025-11-29 11:15:01 +0100

    Robotkar követés kurzorral & robotkar design

--- robotkar.py
+++ robotkar.py
@@ -82,6 +82,7 @@ class RobotApp:
         self.is_moving = False
         self.is_paused = False
         self.anim_queue = []
+        self.mouse_ik_angles = None 
 
         # Layout
         self.left_frame = tk.Frame(root, width=320, bg="#f0f0f0", padx=10, pady=10)
@@ -102,6 +103,8 @@ class RobotApp:
         self.scale = 100.0
 
         self.canvas.bind("<Button-1>", self.on_canvas_click)
+        # Egérmozgás eseménykötése (élő IK)
+        self.canvas.bind("<Motion>", self.on_mouse_move) 
         self.root.bind("<Configure>", self.on_resize)
         
         self.update_model_from_ui()
@@ -159,7 +162,6 @@ class RobotApp:
         tk.Entry(sub, textvariable=self.div_var, width=5).grid(row=1, column=1)
 
     def update_model_from_ui(self, e=None):
-        
         try:
             v = [float(x.get()) for x in self.entries]
             self.model.l3, self.model.l4 = v[0], v[1]
@@ -170,6 +172,25 @@ class RobotApp:
             self.draw()
         except: pass
 
+    # Egérkövetés kezelője
+    def on_mouse_move(self, e):
+        if self.is_moving:
+            return
+            
+        # Egér koordináták átváltása munkaterületi koordinátákra (m)
+        wx = (e.x - self.origin_x) / self.scale
+        wy = (self.origin_y - e.y) / self.scale
+        
+        # Inverz Kinematika futtatása
+        res = self.model.inverse_kinematics(wx, wy)
+        
+        if res:
+            self.mouse_ik_angles = res
+        else:
+            self.mouse_ik_angles = None
+
+        self.draw()
+
     def on_canvas_click(self, e):
         if self.is_moving: return
         wx = (e.x - self.origin_x) / self.scale
@@ -178,6 +199,8 @@ class RobotApp:
         res = self.model.inverse_kinematics(wx, wy)
         if res:
             self.model.points.append((wx, wy, res[0], res[1]))
+            # Töröljük az egérkövetést, hogy a kar a rögzített pontra ugorjon
+            self.mouse_ik_angles = None 
             self.draw()
         else:
             print("Hiba: A pont kívül esik a munkaterületen!")
@@ -188,40 +211,52 @@ class RobotApp:
     def draw(self):
         self.canvas.delete("all")
         bx, by = self.to_scr(0, 0)
+        sA = self.to_scr(0,0)
 
         # --- 1. KOORDINÁTA RENDSZER (GRID és TENGELYEK) ---
         grid_color = "#E0E0E0"
+        axis_color = "#444444" # Sötétszürke a letisztultabb megjelenésért
         max_coord = 6
         axis_length = max_coord * self.scale
         
         # 1.1 Rács (Grid) kirajzolása
         for i in range(1, max_coord):
-            # Függőleges vonalak
             sx = bx + i * self.scale
-            # A rács a teljes vászonra kiterjed
             self.canvas.create_line(sx, 0, sx, self.canvas.winfo_height(), fill=grid_color) 
-            # Vízszintes vonalak
             sy = by - i * self.scale
             self.canvas.create_line(0, sy, self.canvas.winfo_width(), sy, fill=grid_color) 
 
         # 1.2 Tengelyek rajzolása
-        self.canvas.create_line(bx, by, bx + axis_length, by, fill="black", arrow="last", width=1.5)
-        self.canvas.create_line(bx, by, bx, by - axis_length, fill="black", arrow="last", width=1.5)
+        self.canvas.create_line(bx, by, bx + axis_length, by, fill=axis_color, arrow="last", width=1.5)
+        self.canvas.create_line(bx, by, bx, by - axis_length, fill=axis_color, arrow="last", width=1.5)
         
         # Skála Jelölések és feliratok
         for i in range(1, max_coord):
             sx = bx + i * self.scale
-            self.canvas.create_line(sx, by - 5, sx, by + 5, fill="black") 
+            self.canvas.create_line(sx, by - 5, sx, by + 5, fill=axis_color) 
             self.canvas.create_text(sx, by + 15, text=str(i), font=("Arial", 9))
 
             sy = by - i * self.scale
-            self.canvas.create_line(bx - 5, sy, bx + 5, sy, fill="black") 
+            self.canvas.create_line(bx - 5, sy, bx + 5, sy, fill=axis_color) 
             self.canvas.create_text(bx - 15, sy, text=str(i), font=("Arial", 9))
             
         # Tengelyek elnevezése
         self.canvas.create_text(bx + axis_length + 20, by, text="X (m)", font=("Arial", 10, "bold"))
         self.canvas.create_text(bx, by - axis_length - 20, text="Y (m)", font=("Arial", 10, "bold"))
-        self.canvas.create_text(bx - 10, by + 10, text="O", font=("Arial", 10)) 
+        self.canvas.create_text(bx - 10, by + 10, text="O", font=("Arial", 10)) # Origó jelölése
+
+        # --- 1.3. 3D Talapzat (Base Pedestal) az Origónál (A pont) ---
+        # Kisebb méret, hogy ne takarja ki a tengely feliratát
+        pedestal_w, pedestal_h = 20, 7 
+        # Talapzat oldala (Sötét szürke)
+        self.canvas.create_rectangle(sA[0] - pedestal_w, sA[1] - pedestal_h, 
+                                     sA[0] + pedestal_w, sA[1] + pedestal_h, 
+                                     fill="#666666", outline="#333333")
+        # Talapzat teteje (Fekete) - 3D illúzió
+        self.canvas.create_polygon([sA[0] - pedestal_w, sA[1] - pedestal_h, 
+                                    sA[0], sA[1] - pedestal_h - 10, 
+                                    sA[0] + pedestal_w, sA[1] - pedestal_h],
+                                    fill="#444444", outline="#333333")
 
 
         # --- 2. MUNKATERÜLET ---
@@ -248,7 +283,8 @@ class RobotApp:
             sc = self.to_scr(*p)
             pts.extend(sc)
         
-        self.canvas.create_polygon(pts, fill="#9BCEEC", outline="black", dash=(5, 2))
+        # Világosabb munkaterület a letisztult design érdekében
+        self.canvas.create_polygon(pts, fill="#EFEFEF", outline="gray", dash=(5, 2))
 
         # --- Végpontok (A, B, C, D) ---
         _, _, pA_end = self.model.forward_kinematics(self.model.rad_max32, self.model.rad_max43)
@@ -267,68 +303,141 @@ class RobotApp:
         sd = self.to_scr(*pD_end)
         self.canvas.create_text(sd[0], sd[1]+15, text="D", font=("bold"))
 
-        # --- 3. KAR RAJZOLÁSA (Vastagítás és színek) ---
-        p32, p43 = self.model.rad_min32, self.model.rad_min43
-        if self.model.points: p32, p43 = self.model.points[-1][2], self.model.points[-1][3]
-        if hasattr(self, 'anim_st'): p32, p43 = self.anim_st
+        # --- 3. KAR RAJZOLÁSA (Egérkövetéssel és 3D hatással) ---
+        
+        target_p32, target_p43 = self.model.rad_min32, self.model.rad_min43
+        is_tracking = False
+        
+        if self.mouse_ik_angles and not self.is_moving:
+            target_p32, target_p43 = self.mouse_ik_angles
+            is_tracking = True
+            
+        elif self.model.points and not self.is_moving: 
+            target_p32, target_p43 = self.model.points[-1][2], self.model.points[-1][3]
+            
+        elif hasattr(self, 'anim_st') and self.is_moving:
+            target_p32, target_p43 = self.anim_st
 
-        _, pB, pC = self.model.forward_kinematics(p32, p43)
-        sA = self.to_scr(0,0)
+
+        # KAR VIZUALIZÁCIÓ 
+        _, pB, pC = self.model.forward_kinematics(target_p32, target_p43)
         sB = self.to_scr(*pB)
         sC = self.to_scr(*pC)
 
         kar_vastagsag = 8
+        shadow_offset = 4 
         
-       
+        #  Színek
+        fill_L3 = "#C0C0C0" 
+        fill_L4 = "#A9A9A9" 
+        L3_highlight = "#FFFFFF" 
+        L4_highlight = "#D3D3D3" 
+        if is_tracking:
+            fill_L3 = "#AAAAAA" 
+            fill_L4 = "#888888" 
+            dash_style = (2, 4) 
+            kar_vastagsag = 5
+            shadow_offset = 2
+        else:
+            dash_style = () 
+        
+        # ---  ÁRNYÉK ---
+        shadow_color = "#AAAAAA" 
+        
+        # Árnyék L3
+        self.canvas.create_line(sA[0] + shadow_offset, sA[1] + shadow_offset, 
+                                sB[0] + shadow_offset, sB[1] + shadow_offset, 
+                                width=kar_vastagsag, fill=shadow_color, 
+                                capstyle=tk.ROUND, dash=dash_style, tags="robot_shadow")
+        # Árnyék L4
+        self.canvas.create_line(sB[0] + shadow_offset, sB[1] + shadow_offset, 
+                                sC[0] + shadow_offset, sC[1] + shadow_offset, 
+                                width=kar_vastagsag - 2, fill=shadow_color, 
+                                capstyle=tk.ROUND, dash=dash_style, tags="robot_shadow")
+
+
+        # --- 3.2. KAROK RAJZOLÁSA ÉS 3D SHADING ---
+        
+        # L3 Kar (fő vonal)
         self.canvas.create_line(sA[0], sA[1], sB[0], sB[1], 
-                                width=kar_vastagsag, fill="#8B4513", 
-                                capstyle=tk.ROUND)
-        self.canvas.create_text((sA[0]+sB[0])/2, (sA[1]+sB[1])/2 - 15, 
-                                text="L3", font=("Arial", 10, "bold"), fill="black")
-                                
-       
-        self.canvas.create_line(sB[0], sB[1], sC[0], sC[1], 
-                                width=kar_vastagsag - 2, fill="#4682B4", 
-                                capstyle=tk.ROUND)
-        self.canvas.create_text((sB[0]+sC[0])/2 + 15, (sB[1]+sC[1])/2 - 15, 
-                                text="L4", font=("Arial", 10, "bold"), fill="black")
-
+                                width=kar_vastagsag, fill=fill_L3, 
+                                capstyle=tk.ROUND, dash=dash_style, tags="robot_arm")
         
-        self.canvas.create_oval(sA[0]-6, sA[1]-6, sA[0]+6, sA[1]+6, fill="#A9A9A9", outline="black") # A pont (origó)
-        self.canvas.create_oval(sB[0]-6, sB[1]-6, sB[0]+6, sB[1]+6, fill="silver", outline="black") # B pont (könyök)
-        self.canvas.create_oval(sC[0]-8, sC[1]-8, sC[0]+8, sC[1]+8, fill="red", outline="black", tags="robot_end_effector") # C pont (végpont)
+        if not is_tracking:
+            self.canvas.create_line(sA[0]-2, sA[1]-2, sB[0]-2, sB[1]-2,
+                                    width=kar_vastagsag/3, fill=L3_highlight, 
+                                    capstyle=tk.ROUND, tags="robot_arm_shading")
 
-        d32 = math.degrees(p32)
+        self.canvas.create_text((sA[0]+sB[0])/2, (sA[1]+sB[1])/2 - 15, 
+                                text="L3", font=("Arial", 10, "bold"), fill="black", tags="robot_arm")
+                                
+        # L4 Kar (fő vonal)
+        self.canvas.create_line(sB[0], sB[1], sC[0], sC[1], 
+                                width=kar_vastagsag - 2, fill=fill_L4, 
+                                capstyle=tk.ROUND, dash=dash_style, tags="robot_arm")
+                                
+        if not is_tracking:
+            self.canvas.create_line(sB[0]-2, sB[1]-2, sC[0]-2, sC[1]-2,
+                                    width=(kar_vastagsag-2)/3, fill=L4_highlight, 
+                                    capstyle=tk.ROUND, tags="robot_arm_shading")
+                                    
+        self.canvas.create_text((sB[0]+sC[0])/2 + 15, (sB[1]+sC[1])/2 - 15, 
+                                text="L4", font=("Arial", 10, "bold"), fill="black", tags="robot_arm")
+
+        # ÍZÜLETEK 
+        joint_size = 6
+        end_effector_size = 8
+        
+        # A pont (Origó)
+        self.canvas.create_oval(sA[0]-joint_size, sA[1]-joint_size, sA[0]+joint_size, sA[1]+joint_size, fill="#696969", outline="black")
+        self.canvas.create_oval(sA[0]-joint_size+1, sA[1]-joint_size+1, sA[0]+joint_size-3, sA[1]+joint_size-3, fill="#D3D3D3", outline="") 
+
+        # B pont (Könyök)
+        self.canvas.create_oval(sB[0]-joint_size, sB[1]-joint_size, sB[0]+joint_size, sB[1]+joint_size, fill="#888888", outline="black")
+        self.canvas.create_oval(sB[0]-joint_size+1, sB[1]-joint_size+1, sB[0]+joint_size-3, sB[1]+joint_size-3, fill="#EEEEEE", outline="") 
+
+        # C pont (Végpont)
+        self.canvas.create_oval(sC[0]-end_effector_size, sC[1]-end_effector_size, sC[0]+end_effector_size, sC[1]+end_effector_size, fill="red", outline="black", tags="robot_end_effector")
+        self.canvas.create_oval(sC[0]-end_effector_size+2, sC[1]-end_effector_size+2, sC[0]+end_effector_size-4, sC[1]+end_effector_size-4, fill="#FFCCCC", outline="", tags="robot_end_effector")
+
+
+        # --- 4. SZÖG KIJELZÉSEK ÉS ÚTVONAL NYOMKÖVETÉS ---
+        d32 = math.degrees(target_p32)
         self.draw_cs(sB[0], sB[1], d32)
 
-        d43 = math.degrees(p43)
-        self.canvas.create_arc(sA[0]-25, sA[1]-25, sA[0]+25, sA[1]+25, start=0, extent=d32, style="arc", outline="blue")
-        self.canvas.create_text(sA[0]+35, sA[1]-10, text=f"{d32:.1f}°", fill="blue")
+        d43 = math.degrees(target_p43)
+        
+        # phi32 kiírása 
+        self.canvas.create_arc(sA[0]-25, sA[1]-25, sA[0]+25, sA[1]+25, start=0, extent=d32, style="arc", outline="#FF0000", width=2, tags="robot_arm")
+        self.canvas.create_text(sA[0]+35, sA[1]-10, text=f"{d32:.1f}°", fill="#FF0000", font=("Arial", 10, "bold"), tags="robot_arm")
         
         start_angle_b = d32
-        self.canvas.create_arc(sB[0]-25, sB[1]-25, sB[0]+25, sB[1]+25, start=start_angle_b, extent=-d43, style="arc", outline="green")
-        self.canvas.create_text(sB[0]+20, sB[1]+20, text=f"{d43:.1f}°", fill="green")
+        # phi43 kiírása 
+        self.canvas.create_arc(sB[0]-25, sB[1]-25, sB[0]+25, sB[1]+25, start=start_angle_b, extent=-d43, style="arc", outline="#FF0000", width=2, tags="robot_arm")
+        self.canvas.create_text(sB[0]+20, sB[1]+20, text=f"{d43:.1f}°", fill="#FF0000", font=("Arial", 10, "bold"), tags="robot_arm")
 
-        # --- 4. ÚTVONAL NYOMKÖVETÉS---
         for i, p in enumerate(self.model.points):
             sc = self.to_scr(p[0], p[1])
             
-            # Pontok (végpontok)
-            self.canvas.create_oval(sc[0]-4, sc[1]-4, sc[0]+4, sc[1]+4, fill="darkgreen", outline="black")
+            # Pontok 
+            self.canvas.create_oval(sc[0]-4, sc[1]-4, sc[0]+4, sc[1]+4, fill="#FFA500", outline="black")
             
             # Sorszám megjelenítése
-            self.canvas.create_text(sc[0] + 10, sc[1] - 10, text=str(i+1), font=("Arial", 8, "bold"), fill="darkgreen")
+            self.canvas.create_text(sc[0] + 10, sc[1] - 10, text=str(i+1), font=("Arial", 8, "bold"), fill="#FFA500")
             
             if i > 0:
                 prev = self.to_scr(self.model.points[i-1][0], self.model.points[i-1][1])
+                
                 self.canvas.create_line(prev[0], prev[1], sc[0], sc[1], width=2, fill="gray")
 
     def draw_cs(self, ox, oy, ang):
         rad = math.radians(ang)
         l = 30
+        
         x2 = ox + l * math.cos(rad)
         y2 = oy - l * math.sin(rad)
         self.canvas.create_line(ox, oy, x2, y2, fill="red", arrow="last")
+        
         
         rad2 = rad - math.pi/2 
         x3 = ox + l * math.cos(rad2)
@@ -382,7 +491,7 @@ class RobotApp:
                         total_segment_time = (2 * t_acc) + (d_const / v_max)
                         time_at_step = total_segment_time - math.sqrt(2 * rem_dist / acc)
 
-                # Minden szakasz 0-ról indul az időben
+                
                 final_time = time_at_step 
                 self.anim_q.append((curr32, curr43, j, final_time))
 
@@ -393,7 +502,7 @@ class RobotApp:
             self.is_moving=False
             if hasattr(self,'anim_st'): del self.anim_st
             self.draw()
-            # Törli a mozgás közbeni adatkijelzést, amikor a robot megáll
+    
             self.canvas.delete("current_data") 
             return
         
@@ -413,10 +522,9 @@ class RobotApp:
         self.tree.yview_moveto(1)
         self.draw()
         
-        # --- Pillanatnyi adatok megjelenítése a végpontnál ---
         sC = self.to_scr(*pos)
         current_data = f"X: {pos[0]:.2f} m\nY: {pos[1]:.2f} m\nT: {data[3]:.2f} s"
-        
+       
         self.canvas.create_text(sC[0] + 50, sC[1] - 20, 
                                 text=current_data, 
                                 fill="red", font=("Arial", 10, "bold"), 
